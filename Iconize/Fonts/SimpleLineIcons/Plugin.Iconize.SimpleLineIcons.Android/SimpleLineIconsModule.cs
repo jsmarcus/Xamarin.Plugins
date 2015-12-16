d71504774546938e1c46fdc381fc3a7ea93615fa
Jeremy Marcus <jeremymarcus@msn.com>
2015-12-16 15:14:48 -0500

    Added Simple Line Icons collection

---
+++ Iconize/Fonts/SimpleLineIcons/Plugin.Iconize.SimpleLineIcons.Android/SimpleLineIconsModule.cs
@@ -0,0 +1,11 @@
+namespace Plugin.Iconize.Fonts
+{
+    public sealed class SimpleLineIconsModule : IconModule
+    {
+        public SimpleLineIconsModule()
+            : base("Simple Line Icons", "simple-line-icons", "Iconize/iconize-simplelineicons.ttf", SimpleLineIconsCollection.Icons)
+        {
+            // Intentionally left blank
+        }
+    }
+}
---
+++ Iconize/Fonts/SimpleLineIcons/Plugin.Iconize.SimpleLineIcons.iOS/SimpleLineIconsModule.cs
@@ -0,0 +1,11 @@
+﻿namespace Plugin.Iconize.Fonts
+{
+    public sealed class SimpleLineIconsModule : IconModule
+    {
+        public SimpleLineIconsModule()
+            : base("Simple Line Icons", "simple-line-icons", "iconize-simplelineicons.ttf", SimpleLineIconsCollection.Icons)
+        {
+            // Intentionally left blank
+        }
+    }
+}
