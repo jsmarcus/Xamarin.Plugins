ab56fe4f9a1f2561a7cbc23641e3e80b91b123c5
Jeremy Marcus <jeremymarcus@msn.com>
2015-12-16 09:44:13 -0500

    Added Ionicons to Iconize

---
+++ Iconize/Fonts/Ionicons/Plugin.Iconize.Ionicons.Android/IoniconsModule.cs
@@ -0,0 +1,11 @@
+namespace Plugin.Iconize.Fonts
+{
+    public sealed class IoniconsModule : IconModule
+    {
+        public IoniconsModule()
+            : base("Ionicons", "Ionicons", "Iconize/iconize-ionicons.ttf", IoniconsCollection.Icons)
+        {
+            // Intentionally left blank
+        }
+    }
+}
---
+++ Iconize/Fonts/Ionicons/Plugin.Iconize.Ionicons.iOS/IoniconsModule.cs
@@ -0,0 +1,11 @@
+﻿namespace Plugin.Iconize.Fonts
+{
+    public sealed class IoniconsModule : IconModule
+    {
+        public IoniconsModule()
+            : base("Ionicons", "Ionicons", "iconize-ionicons.ttf", IoniconsCollection.Icons)
+        {
+            // Intentionally left blank
+        }
+    }
+}
