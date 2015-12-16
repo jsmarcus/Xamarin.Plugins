2da7ad2d496576cba63ad48231b65b53abeb24ff
Jeremy Marcus <jeremymarcus@msn.com>
2015-12-16 10:39:50 -0500

    Added Weather Icons module to Iconize

---
+++ Iconize/Fonts/WeatherIcons/Plugin.Iconize.WeatherIcons.Android/WeatherIconsModule.cs
@@ -0,0 +1,11 @@
+namespace Plugin.Iconize.Fonts
+{
+    public sealed class WeatherIconsModule : IconModule
+    {
+        public WeatherIconsModule()
+            : base("Weather Icons", "Weather Icons", "Iconize/iconize-weathericons.ttf", WeatherIconsCollection.Icons)
+        {
+            // Intentionally left blank
+        }
+    }
+}
---
+++ Iconize/Fonts/WeatherIcons/Plugin.Iconize.WeatherIcons.iOS/WeatherIconsModule.cs
@@ -0,0 +1,11 @@
+﻿namespace Plugin.Iconize.Fonts
+{
+    public sealed class WeatherIconsModule : IconModule
+    {
+        public WeatherIconsModule()
+            : base("Weather Icons", "Weather Icons", "iconize-weathericons.ttf", WeatherIconsCollection.Icons)
+        {
+            // Intentionally left blank
+        }
+    }
+}
