df57cb9351de6f43daa14f445a43feab5e2403af
Riccardo Marraghini <riccardo.marraghini@gmail.com>
2016-04-16 14:13:27 +0200

    Indentation Fixes

--- Iconize/FormsPlugin.Iconize.iOS/IconImageRenderer.cs
+++ Iconize/FormsPlugin.Iconize.iOS/IconImageRenderer.cs
@@ -10,67 +10,73 @@ using Xamarin.Forms.Platform.iOS;
 [assembly: ExportRenderer(typeof(IconImage), typeof(IconImageRenderer))]
 namespace FormsPlugin.Iconize.iOS
 {
-	/// <summary>
-	/// Defines the <see cref="IconImageRenderer" /> renderer.
-	/// </summary>
-	/// <seealso cref="Xamarin.Forms.Platform.iOS.ImageRenderer" />
-	public class IconImageRenderer : ImageRenderer
-	{
-		/// <summary>
-		/// Raises the <see cref="E:ElementChanged" /> event.
-		/// </summary>
-		/// <param name="e">The <see cref="ElementChangedEventArgs{Image}" /> instance containing the event data.</param>
-		protected override void OnElementChanged(ElementChangedEventArgs<Image> e)
-		{
-			base.OnElementChanged(e);
+    /// <summary>
+    /// Defines the <see cref="IconImageRenderer" /> renderer.
+    /// </summary>
+    /// <seealso cref="Xamarin.Forms.Platform.iOS.ImageRenderer" />
+    public class IconImageRenderer : ImageRenderer
+    {
+        /// <summary>
+        /// Raises the <see cref="E:ElementChanged" /> event.
+        /// </summary>
+        /// <param name="e">The <see cref="ElementChangedEventArgs{Image}" /> instance containing the event data.</param>
+        protected override void OnElementChanged(ElementChangedEventArgs<Image> e)
+        {
+            base.OnElementChanged(e);
 
-			if (Control == null || Element == null)
-				return;
+            if (Control == null || Element == null)
+                return;
 
-			this.UpdateImage(true);
-		}
+            this.UpdateImage(true);
+        }
 
-		/// <summary>
-		/// Called when [element property changed].
-		/// </summary>
-		/// <param name="sender">The sender.</param>
-		/// <param name="e">The <see cref="PropertyChangedEventArgs" /> instance containing the event data.</param>
-		protected override void OnElementPropertyChanged(Object sender, PropertyChangedEventArgs e)
-		{
-			base.OnElementPropertyChanged(sender, e);
+        /// <summary>
+        /// Called when [element property changed].
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The <see cref="PropertyChangedEventArgs" /> instance containing the event data.</param>
+        protected override void OnElementPropertyChanged(Object sender, PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
 
-			if (Control == null || Element == null)
-				return;
+            if (Control == null || Element == null)
+                return;
 
-			switch (e.PropertyName)
-			{
-				case nameof(IconImage.Icon):
-				case nameof(IconImage.IconSize):
-					this.UpdateImage(true);
-					break;
+            switch (e.PropertyName)
+            {
+                case nameof(IconImage.Icon):
+                case nameof(IconImage.IconSize):
+                    this.UpdateImage(true);
+                    break;
 
-				case nameof(IconImage.IconColor):
-					this.UpdateImage(false);
-					break;
-			}
-		}
+                case nameof(IconImage.IconColor):
+                    this.UpdateImage(false);
+                    break;
+            }
+        }
 
-		private void UpdateImage(bool shouldUpdateImage)
-		{
-			var iconImage = Element as IconImage;
-			if (shouldUpdateImage)
-			{
-				Control.ContentMode =
-					iconImage.IconSize > 0 ?
-						UIViewContentMode.Center :
-						UIViewContentMode.ScaleAspectFit;
-				Control.Image =
-					Plugin.Iconize.Iconize
-						.FindIconForKey(iconImage.Icon)
-						.ToUIImage(iconImage.IconSize > 0 ? (nfloat)iconImage.IconSize : (nfloat)Element.HeightRequest)
-						.ImageWithRenderingMode(UIImageRenderingMode.AlwaysTemplate);
-			}
-			Control.TintColor = iconImage.IconColor.ToUIColor();
-		}
-	}
+        private void UpdateImage(bool shouldUpdateImage)
+        {
+            var iconImage = Element as IconImage;
+            if (shouldUpdateImage)
+            {
+                Control.ContentMode =
+                        iconImage.IconSize > 0 ?
+                            UIViewContentMode.Center :
+                            UIViewContentMode.ScaleAspectFit;
+
+                var iconToDraw = Plugin.Iconize.Iconize.FindIconForKey(iconImage.Icon);
+                if (iconToDraw == null)
+                {
+                    Control.Image = null;
+                    return;
+                }
+                Control.Image =
+                        iconToDraw
+                        .ToUIImage(iconImage.IconSize > 0 ? (nfloat)iconImage.IconSize : (nfloat)Element.HeightRequest)
+                        .ImageWithRenderingMode(UIImageRenderingMode.AlwaysTemplate);
+            }
+            Control.TintColor = iconImage.IconColor.ToUIColor();
+        }
+    }
 }
--- Iconize/FormsPlugin.Iconize/IconImage.cs
+++ Iconize/FormsPlugin.Iconize/IconImage.cs
@@ -3,73 +3,73 @@ using Xamarin.Forms;
 
 namespace FormsPlugin.Iconize
 {
-	/// <summary>
-	/// Defines the <see cref="IconImage" /> control.
-	/// </summary>
-	/// <seealso cref="Xamarin.Forms.Image" />
-	public class IconImage : Image
-	{
-		#region Static Properties
+    /// <summary>
+    /// Defines the <see cref="IconImage" /> control.
+    /// </summary>
+    /// <seealso cref="Xamarin.Forms.Image" />
+    public class IconImage : Image
+    {
+        #region Static Properties
 
-		/// <summary>
-		/// When the property <see cref="IconSize" /> is set to this value, the icon size will match the Image Size.
-		/// </summary>
-		public static double IconAutoSize { get { return -1.0; } }
+        /// <summary>
+        /// When the property <see cref="IconSize" /> is set to this value, the icon size will match the Image Size.
+        /// </summary>
+        public static double IconAutoSize { get { return -1.0; } }
 
-		#endregion
+        #endregion
 
-		#region Icon Property
+        #region Icon Property
 
-		/// <summary>
-		/// Property definition for the <see cref="Icon" /> Property
-		/// </summary>
-		public static readonly BindableProperty IconProperty = BindableProperty.Create(nameof(Icon), typeof(String), typeof(IconImage), String.Empty);
+        /// <summary>
+        /// Property definition for the <see cref="Icon" /> Property
+        /// </summary>
+        public static readonly BindableProperty IconProperty = BindableProperty.Create(nameof(Icon), typeof(String), typeof(IconImage), String.Empty);
 
-		/// <summary>
-		/// Gets or sets the icon.
-		/// </summary>
-		/// <value>
-		/// The icon.
-		/// </value>
-		public String Icon
-		{
-			get { return (String)GetValue(IconProperty); }
-			set { SetValue(IconProperty, value); }
-		}
+        /// <summary>
+        /// Gets or sets the icon.
+        /// </summary>
+        /// <value>
+        /// The icon.
+        /// </value>
+        public String Icon
+        {
+            get { return (String)GetValue(IconProperty); }
+            set { SetValue(IconProperty, value); }
+        }
 
-		#endregion
+        #endregion
 
-		#region IconColor Property
+        #region IconColor Property
 
-		/// <summary>
-		/// Property definition for the <see cref="IconColor" /> Property
-		/// </summary>
-		public static BindableProperty IconColorProperty = BindableProperty.Create(nameof(IconColor), typeof(Color), typeof(IconImage), Color.Default);
+        /// <summary>
+        /// Property definition for the <see cref="IconColor" /> Property
+        /// </summary>
+        public static BindableProperty IconColorProperty = BindableProperty.Create(nameof(IconColor), typeof(Color), typeof(IconImage), Color.Default);
 
-		/// <summary>
-		/// Gets or sets the color of the icon.
-		/// </summary>
-		/// <value>
-		/// The color of the icon.
-		/// </value>
-		public Color IconColor
-		{
-			get { return (Color)GetValue(IconColorProperty); }
-			set { SetValue(IconColorProperty, value); }
-		}
+        /// <summary>
+        /// Gets or sets the color of the icon.
+        /// </summary>
+        /// <value>
+        /// The color of the icon.
+        /// </value>
+        public Color IconColor
+        {
+            get { return (Color)GetValue(IconColorProperty); }
+            set { SetValue(IconColorProperty, value); }
+        }
 
-		#endregion
+        #endregion
 
-		#region IconSize Property
+        #region IconSize Property
 
-		public static readonly BindableProperty IconSizeProperty = BindableProperty.Create(nameof(IconSize), typeof(double), typeof(IconImage), IconImage.IconAutoSize);
+        public static readonly BindableProperty IconSizeProperty = BindableProperty.Create(nameof(IconSize), typeof(double), typeof(IconImage), IconImage.IconAutoSize);
 
-		public double IconSize
-		{
-			get { return (double)GetValue(IconSizeProperty); }
-			set { SetValue(IconSizeProperty, value); }
-		}
+        public double IconSize
+        {
+            get { return (double)GetValue(IconSizeProperty); }
+            set { SetValue(IconSizeProperty, value); }
+        }
 
-		#endregion
-	}
+        #endregion
+    }
 }
