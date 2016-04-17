c3146aab7d1335a271f242acd4e6ea42bdebb06c
Marraghini R. <riccardo.marraghini@gmail.com>
2016-04-17 23:05:59 +0200

    prevents LINQ exception if iconKey is null, also prevents a search if iconKey is empty.

--- Iconize/Plugin.Iconize/Iconize.cs
+++ Iconize/Plugin.Iconize/Iconize.cs
@@ -98,6 +98,7 @@ namespace Plugin.Iconize
         /// <returns>The icon, or null if no icon matches the key</returns>
         public static IIcon FindIconForKey(String iconKey)
         {
+            if (string.IsNullOrEmpty(iconKey)) { return null; }
             return Modules.FirstOrDefault(x => x.Keys.Contains(iconKey))?.GetIcon(iconKey);
         }
     }
