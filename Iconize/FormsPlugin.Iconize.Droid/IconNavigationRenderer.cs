e4f86206875f29bc71e87978c92ceba66f0502f0
Jeremy Marcus <jeremymarcus@msn.com>
2016-05-17 14:14:31 -0400

    Fixed an issue with toolbar updates on Android after orientation change

--- Iconize/FormsPlugin.Iconize.Droid/IconNavigationRenderer.cs
+++ Iconize/FormsPlugin.Iconize.Droid/IconNavigationRenderer.cs
@@ -1,8 +1,8 @@
 using System;
+using Android.Content.Res;
 using FormsPlugin.Iconize;
 using FormsPlugin.Iconize.Droid;
 using Xamarin.Forms;
-using Xamarin.Forms.Platform.Android;
 using Xamarin.Forms.Platform.Android.AppCompat;
 
 [assembly: ExportRenderer(typeof(IconNavigationPage), typeof(IconNavigationRenderer))]
@@ -14,30 +14,51 @@ namespace FormsPlugin.Iconize.Droid
     /// <seealso cref="Xamarin.Forms.Platform.Android.AppCompat.NavigationPageRenderer" />
     public class IconNavigationRenderer : NavigationPageRenderer
     {
+        private Orientation _orientation = Orientation.Portrait;
+
         /// <summary>
         /// Called when [attached to window].
         /// </summary>
         protected override void OnAttachedToWindow()
         {
             MessagingCenter.Subscribe<Object>(this, IconToolbarItem.UpdateToolbarItemsMessage, OnUpdateToolbarItems);
-            (Context as FormsAppCompatActivity).ConfigurationChanged += OnConfigurationChanged;
             OnUpdateToolbarItems(this);
 
             base.OnAttachedToWindow();
         }
 
         /// <summary>
-        /// Called when [configuration changed].
+        /// Called when the current configuration of the resources being used
+        /// by the application have changed.
         /// </summary>
-        /// <param name="sender">The sender.</param>
-        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
-        private void OnConfigurationChanged(Object sender, EventArgs e)
+        /// <param name="newConfig">The new resource configuration.</param>
+        /// <remarks>
+        /// <para tool="javadoc-to-mdoc">Called when the current configuration of the resources being used
+        /// by the application have changed.  You can use this to decide when
+        /// to reload resources that can changed based on orientation and other
+        /// configuration characterstics.  You only need to use this if you are
+        /// not relying on the normal <c><see cref="T:Android.App.Activity" /></c> mechanism of
+        /// recreating the activity instance upon a configuration change.</para>
+        /// <para tool="javadoc-to-mdoc">
+        ///   <format type="text/html">
+        ///     <a href="http://developer.android.com/reference/android/view/View.html#onConfigurationChanged(android.content.res.Configuration)" target="_blank">[Android Documentation]</a>
+        ///   </format>
+        /// </para>
+        /// </remarks>
+        /// <since version="Added in API level 8" />
+        protected override void OnConfigurationChanged(Configuration newConfig)
         {
-            Device.StartTimer(TimeSpan.FromMilliseconds(1), () =>
+            base.OnConfigurationChanged(newConfig);
+
+            if (newConfig.Orientation != _orientation)
             {
-                OnUpdateToolbarItems(this);
-                return false;
-            });
+                _orientation = newConfig.Orientation;
+                Device.StartTimer(TimeSpan.FromMilliseconds(100), () =>
+                {
+                    OnUpdateToolbarItems(this);
+                    return false;
+                });
+            }
         }
 
         /// <summary>
@@ -47,7 +68,6 @@ namespace FormsPlugin.Iconize.Droid
         {
             base.OnDetachedFromWindow();
 
-            (Context as FormsAppCompatActivity).ConfigurationChanged -= OnConfigurationChanged;
             MessagingCenter.Unsubscribe<Object>(this, IconToolbarItem.UpdateToolbarItemsMessage);
         }
 
